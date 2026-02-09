06aea0dfb477d4816b3194d3696ee5c08528c9e6
EliteHomes38 <burnsadrian38@gmail.com>
2026-02-09 11:20:00 -0600

    Add files via upload

---
+++ Administratives/Create-O365Group-AdHoc.ps1
@@ -0,0 +1,29 @@
+﻿$members = cat C:\Mig\WSILLC.txt
+$owner = "lpolzin@AcrisureLLC.com"
+$groupname = "zPresidio"
+$GroupEmail = "zpresidio@presidiogrp.com"
+   
+   
+    try {
+
+       
+        New-UnifiedGroup -DisplayName $groupname -PrimarySmtpAddress $GroupEmail -RequireSenderAuthenticationEnabled:$false -Owner $owner -AutoSubscribeNewMembers $true
+        Set-UnifiedGroup -Identity $GroupName -UnifiedGroupWelcomeMessageEnabled:$false
+    }
+    catch {
+        $ok = $false
+        Write-Warning "Error creating a new group for $group.name $($Error[0].Exception)" 
+    
+    }
+    $newmembers = Foreach ($member in $members) {
+        Write-host "checking $member" -ForegroundColor Green;
+        if ($member -ne $null) {
+            (Get-mailbox $member -ea SilentlyContinue -RecipientTypeDetails UserMailbox).UserPrincipalname
+        }
+    }
+    if ($newmembers) {
+        Add-UnifiedGroupLinks -Identity $GroupName -LinkType Members -Links ($newmembers | select -Unique)
+             
+        Write-Host "New members added to $GroupName successfully" -ForegroundColor Green
+    
+    }
